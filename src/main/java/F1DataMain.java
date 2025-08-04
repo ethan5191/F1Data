07cd81372c07167510362afb79fdb0d73234bc9c
ethan5191 <ethan5191@hotmail.com>
2025-08-04 14:23:37 -0400

    Moved logic into its own methods to better breakup code. Created a run method which is being called from the main method

--- src/main/java/F1DataMain.java
+++ src/main/java/F1DataMain.java
@@ -14,53 +14,32 @@ import java.util.Map;
 
 public class F1DataMain {
 
-    public static void main(String[] args) {
+    private final Map<Integer, ParticipantData> participants = new HashMap<>();
+
+    public void run() {
         int port = Constants.PORT_NUM;
         byte[] buffer = new byte[2048];
         try {
             DatagramSocket socket = new DatagramSocket(port);
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-            Map<Integer, ParticipantData> participants = new HashMap<>();
             while (true) {
                 socket.receive(packet);
                 int length = packet.getLength();
                 ByteBuffer byteBuffer = ByteBuffer.wrap(Arrays.copyOfRange(buffer, 0, length));
                 byteBuffer.order(ByteOrder.LITTLE_ENDIAN);
                 PacketHeader ph = new PacketHeader(byteBuffer);
-//                if (ph.getPacketId() == Constants.MOTION_PACK) {
-//                    packets.MotionData md = new packets.MotionData(byteBuffer, ph.getPlayerCarIndex());
-//                    System.out.println("Forward X " + md.getWorldForwardDirX() + " Forward Y " + md.getWorldForwardDirY() + " Forward Z " + md.getWorldForwardDirZ());
-//                }
-                if (ph.getPacketId() == Constants.LAP_DATA_PACK && !participants.isEmpty()) {
-                    for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
-                        LapData ld = new LapData(byteBuffer);
-                        if (validKey(participants, i)) {
-                            System.out.println(ld.getCurrentLapNum() + " " + ld.getSpeedTrapFastestSpeed() + " " + ld.getCarPosition() + " " + ld.getLapDistance() + " " + ld.getTotalDistance());
-                        }
-                    }
-                    //Time trail params at the end of the Lap Data packet. Only there a single time, therefore they are outside of the loop.
-                    byte timeTrailPBCarId = byteBuffer.get();
-                    byte timeTrailRivalPdCarId = byteBuffer.get();
-                } else if (ph.getPacketId() == Constants.CAR_SETUP_PACK && !participants.isEmpty()) {
-                    for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
-                        CarSetupData csd = new CarSetupData(byteBuffer);
-                        if (validKey(participants, i)) {
-                            System.out.println("I " + i + " Front Wing " + csd.getFrontWing() + " Rear " + csd.getRearWing());
-                        }
-                    }
-                } else if (ph.getPacketId() == Constants.PARTICIPANTS_PACK && participants.isEmpty()) {
-                    int index = 0;
-                    //DO NOT DELETE THIS LINE, you will break the logic below it, we have to move the position with the .get() for the logic to work.
-                    int numActiveCars = byteBuffer.get();
-                    for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
-                        ParticipantData pd = new ParticipantData(byteBuffer);
-                        if (pd.getRaceNumber() > 0) {
-                            pd.printName();
-                            participants.put(index, pd);
-                        }
-                        //CRUCIAL!!!!!!! index value ensures that the drivers will be mapped to the correct lap data.
-                        index++;
-                    }
+                switch (ph.getPacketId()) {
+                    case Constants.MOTION_PACK:
+                        break;
+                    case Constants.LAP_DATA_PACK:
+                        handleLapDataPacket(byteBuffer);
+                        break;
+                    case Constants.CAR_SETUP_PACK:
+                        handleCarSetupPacket(byteBuffer);
+                        break;
+                    case Constants.PARTICIPANTS_PACK:
+                        handleParticipantsPacket(byteBuffer);
+                        break;
                 }
             }
         } catch (IOException e) {
@@ -69,7 +48,47 @@ public class F1DataMain {
     }
 
     //Checks if the map of participants(drivers in session) contains the id we are looking for. Prevents extra ids for custom team from printing stuff when they have no data.
-    private static boolean validKey(Map<Integer, ParticipantData> participants, int i) {
+    private boolean validKey(Map<Integer, ParticipantData> participants, int i) {
         return participants.containsKey(i);
+    }
+
+    private void handleLapDataPacket(ByteBuffer byteBuffer) {
+        for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
+            LapData ld = new LapData(byteBuffer);
+            if (validKey(participants, i)) {
+                System.out.println(ld.getCurrentLapNum() + " " + ld.getSpeedTrapFastestSpeed() + " " + ld.getCarPosition() + " " + ld.getLapDistance() + " " + ld.getTotalDistance());
+            }
+        }
+        //Time trail params at the end of the Lap Data packet. Only there a single time, therefore they are outside of the loop.
+        byte timeTrailPBCarId = byteBuffer.get();
+        byte timeTrailRivalPdCarId = byteBuffer.get();
+    }
+
+    private void handleCarSetupPacket(ByteBuffer byteBuffer) {
+        for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
+            CarSetupData csd = new CarSetupData(byteBuffer);
+            if (validKey(participants, i)) {
+                System.out.println("I " + i + " Front Wing " + csd.getFrontWing() + " Rear " + csd.getRearWing());
+            }
+        }
+    }
+
+    private void handleParticipantsPacket(ByteBuffer byteBuffer) {
+        int index = 0;
+        //DO NOT DELETE THIS LINE, you will break the logic below it, we have to move the position with the .get() for the logic to work.
+        int numActiveCars = byteBuffer.get();
+        for (int i = 0; i < Constants.PACKET_CAR_COUNT; i++) {
+            ParticipantData pd = new ParticipantData(byteBuffer);
+            if (pd.getRaceNumber() > 0) {
+                pd.printName();
+                participants.put(index, pd);
+            }
+            //CRUCIAL!!!!!!! index value ensures that the drivers will be mapped to the correct lap data.
+            index++;
+        }
+    }
+
+    public static void main(String[] args) {
+        new F1DataMain().run();
     }
 }
