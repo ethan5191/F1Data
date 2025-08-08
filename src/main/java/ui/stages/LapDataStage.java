9c0892a422b9e22d7794ab9e73c8b3d5785a27d1
ethan5191 <ethan5191@hotmail.com>
2025-08-08 08:51:39 -0400

    Updated logic to have the LapData scene take up the entire height by default

--- src/main/java/ui/stages/LapDataStage.java
+++ src/main/java/ui/stages/LapDataStage.java
@@ -1,8 +1,13 @@
 package ui.stages;
 
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import static ui.AllLapDataDashboard.LAP_HEADERS;
 import static ui.AllLapDataDashboard.LAP_HEADERS_WIDTH;
@@ -23,7 +28,17 @@ public class LapDataStage extends AbstractStage<VBox> {
         scroll.setContent(this.content);
         scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
         scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        showStage(300, 700);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        stage.setX(bounds.getMinX());
+        stage.setY(bounds.getMinY());
+        stage.setWidth(300);
+        stage.setHeight(bounds.getHeight());
+        Scene scene = new Scene(this.content);
+        scene.setFill(Color.TRANSPARENT);
+        this.stage.setScene(scene);
+        this.stage.initStyle(StageStyle.TRANSPARENT);
+        this.stage.show();
     }
 
     public VBox createParentContent() {
