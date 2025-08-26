6695ed92b83671d269be811ec1f05c5dfceba733
ethan5191 <ethan5191@hotmail.com>
2025-08-26 08:58:48 -0400

    Moved TeamSpeedTrapStage over to use its own logic to ensure the scrollPanel works. this is a WIP commit as I need to condense the logic from this and the previous commit to reduce redundant code

--- src/main/java/f1/data/ui/stages/TeamSpeedTrapStage.java
+++ src/main/java/f1/data/ui/stages/TeamSpeedTrapStage.java
@@ -1,33 +1,98 @@
 package f1.data.ui.stages;
 
-import javafx.beans.property.BooleanProperty;
-import javafx.scene.layout.VBox;
-import javafx.stage.Stage;
 import f1.data.ui.dashboards.TeamSpeedTrapDashboard;
 import f1.data.ui.home.AppState;
+import f1.data.ui.stages.helper.Delta;
+import javafx.beans.property.BooleanProperty;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
-public class TeamSpeedTrapStage extends AbstractStage<VBox> {
+public class TeamSpeedTrapStage {
 
-    public TeamSpeedTrapStage(Stage stage, VBox teamSpeedTrapData) {
-        super(stage, TeamSpeedTrapDashboard.HEADERS, TeamSpeedTrapDashboard.HEADERS_WIDTH);
-        this.teamSpeedTrapData = teamSpeedTrapData;
+    private final Stage stage;
+    private final VBox content;
+
+    public TeamSpeedTrapStage(Stage stage, VBox content) {
+        this.stage = stage;
+        this.content = content;
+        this.content.setStyle("-fx-background-color: rgba(0, 0, 0, 0.33);");
         init();
     }
 
-    private final VBox teamSpeedTrapData;
-
-    @Override
-    protected VBox createParentContent() {
-        return createParentVbox();
+    private void init() {
+        addDragAndDrop(this.content);
+        Screen screen = Screen.getPrimary();
+        Rectangle2D bounds = screen.getVisualBounds();
+        VBox container = new VBox(3);
+        createHeader(container);
+        container.getChildren().add(createScrollPane(bounds.getHeight()));
+        container.setStyle("-fx-background-color: rgba(0, 0, 0, 0.33);");
+        createScene(container, 250, bounds.getHeight());
     }
 
-    @Override
-    protected void init() {
-        this.content.getChildren().add(teamSpeedTrapData);
-        setFullHeightScene(250);
+    private void addDragAndDrop(Pane element) {
+        Delta dragDelta = new Delta();
+        element.setOnMousePressed(e -> {
+            dragDelta.x = stage.getX() - e.getScreenX();
+            dragDelta.y = stage.getY() - e.getScreenY();
+        });
+        element.setOnMouseDragged(e -> {
+            stage.setX(e.getScreenX() + dragDelta.x);
+            stage.setY(e.getScreenY() + dragDelta.y);
+        });
     }
 
-    @Override
+    private ScrollPane createScrollPane(double height) {
+        ScrollPane pane = new ScrollPane();
+        pane.setMinHeight(height);
+        pane.setStyle(
+                "-fx-background-color: transparent;" +
+                        "-fx-control-inner-background: transparent;" +
+                        "-fx-background: transparent;" +
+                        "-fx-box-border: transparent;" +
+                        "-fx-border-color: transparent;"
+        );
+        pane.setContent(this.content);
+        pane.setPannable(true);
+        pane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        pane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        return pane;
+    }
+
+    private void createHeader(VBox container) {
+        HBox headersBox = new HBox(3);
+        for (int i = 0; i < TeamSpeedTrapDashboard.HEADERS.length; i++) {
+            Label header = new Label(TeamSpeedTrapDashboard.HEADERS[i]);
+            header.setMinWidth(TeamSpeedTrapDashboard.HEADERS_WIDTH[i]);
+            headersBox.getChildren().add(header);
+            header.setTextFill(Color.WHITE);
+        }
+        container.getChildren().add(headersBox);
+    }
+
+    private void createScene(Pane container, double width, double height) {
+        Scene scene = new Scene(container, width, height);
+        scene.setFill(Color.TRANSPARENT);
+        this.stage.setScene(scene);
+        this.stage.initStyle(StageStyle.TRANSPARENT);
+        getAppState().addListener((observable, oldValue, newValue) -> {
+            if (newValue) {
+                this.stage.show();
+            } else if (oldValue) {
+                this.stage.hide();
+            }
+        });
+    }
+
     protected BooleanProperty getAppState() {
         return AppState.teamSpeedTrapPanelVisible;
     }
