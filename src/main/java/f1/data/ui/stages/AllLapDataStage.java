9e935de414d471580fb9b71b3da95a8194a3fd2b
ethan5191 <ethan5191@hotmail.com>
2025-08-26 09:28:37 -0400

    Condensed common logic into an abstract parent class. this will speed up creation of scrollPanel based stages

--- src/main/java/f1/data/ui/stages/AllLapDataStage.java
+++ src/main/java/f1/data/ui/stages/AllLapDataStage.java
@@ -1,99 +1,28 @@
 package f1.data.ui.stages;
 
 import f1.data.ui.dashboards.AllLapDataDashboard;
-import f1.data.ui.stages.helper.Delta;
+import f1.data.ui.home.AppState;
 import javafx.beans.property.BooleanProperty;
 import javafx.geometry.Rectangle2D;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.stage.Screen;
 import javafx.stage.Stage;
-import f1.data.ui.home.AppState;
-import javafx.stage.StageStyle;
 
-public class AllLapDataStage {
-
-    private final Stage stage;
-    private final VBox content;
+public class AllLapDataStage extends AbstractScrollPaneStage {
 
     public AllLapDataStage(Stage stage, VBox content) {
-        this.stage = stage;
-        this.content = content;
-        this.content.setStyle("-fx-background-color: rgba(0, 0, 0, 0.33);");
-        init();
+        super(stage, content, AllLapDataDashboard.LAP_HEADERS, AllLapDataDashboard.LAP_HEADERS_WIDTH);
     }
 
-    private void init() {
-        addDragAndDrop(this.content);
-        Screen screen = Screen.getPrimary();
-        Rectangle2D bounds = screen.getVisualBounds();
-        VBox container = new VBox(3);
-        createHeader(container);
-        container.getChildren().add(createScrollPane(bounds.getHeight()));
-        container.setStyle("-fx-background-color: rgba(0, 0, 0, 0.33);");
-        createScene(container, 300, bounds.getHeight());
+    protected double getSceneWidth() {
+        return 300;
     }
 
-    private void addDragAndDrop(Pane element) {
-        Delta dragDelta = new Delta();
-        element.setOnMousePressed(e -> {
-            dragDelta.x = stage.getX() - e.getScreenX();
-            dragDelta.y = stage.getY() - e.getScreenY();
-        });
-        element.setOnMouseDragged(e -> {
-            stage.setX(e.getScreenX() + dragDelta.x);
-            stage.setY(e.getScreenY() + dragDelta.y);
-        });
+    protected double getSceneHeight() {
+        return useFullScreenHeight();
     }
 
-    private ScrollPane createScrollPane(double height) {
-        ScrollPane pane = new ScrollPane();
-        pane.setMinHeight(height);
-        pane.setStyle(
-                "-fx-background-color: transparent;" +
-                        "-fx-control-inner-background: transparent;" +
-                        "-fx-background: transparent;" +
-                        "-fx-box-border: transparent;" +
-                        "-fx-border-color: transparent;"
-        );
-        pane.setContent(this.content);
-        pane.setPannable(true);
-        pane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
-        pane.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
-        return pane;
-    }
-
-    private void createHeader(VBox container) {
-        HBox headersBox = new HBox(3);
-        for (int i = 0; i < AllLapDataDashboard.LAP_HEADERS.length; i++) {
-            Label header = new Label(AllLapDataDashboard.LAP_HEADERS[i]);
-            header.setMinWidth(AllLapDataDashboard.LAP_HEADERS_WIDTH[i]);
-            headersBox.getChildren().add(header);
-            header.setTextFill(Color.WHITE);
-        }
-        container.getChildren().add(headersBox);
-    }
-
-    private void createScene(Pane container, double width, double height) {
-        Scene scene = new Scene(container, width, height);
-        scene.setFill(Color.TRANSPARENT);
-        this.stage.setScene(scene);
-        this.stage.initStyle(StageStyle.TRANSPARENT);
-        getAppState().addListener((observable, oldValue, newValue) -> {
-            if (newValue) {
-                this.stage.show();
-            } else if (oldValue) {
-                this.stage.hide();
-            }
-        });
-    }
-
-    private BooleanProperty getAppState() {
+    protected BooleanProperty getAppState() {
         return AppState.allLapsDataPanelVisible;
     }
 }
