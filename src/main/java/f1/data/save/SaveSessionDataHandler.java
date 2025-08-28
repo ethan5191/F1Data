b164f144c02d06cec2d8d08a44fb79a959b8a8b1
ethan5191 <ethan5191@hotmail.com>
2025-08-28 15:58:24 -0400

    Added logic to dynamically build the save location to store the save files in the project direcctory wheather running it from an IDE or using the JAR.

--- src/main/java/f1/data/save/SaveSessionDataHandler.java
+++ src/main/java/f1/data/save/SaveSessionDataHandler.java
@@ -4,21 +4,30 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class SaveSessionDataHandler {
 
     private static final Logger logger = LoggerFactory.getLogger(SaveSessionDataHandler.class);
 
     public static void saveSessionData(String sessionName, SpeedTrapSessionWrapper speedTrapSessionWrapper) {
-        String userHome = System.getProperty("user.home");
-        String desktopPath = userHome + File.separator + "Desktop";
-        File myFile = new File(desktopPath, sessionName + ".json");
+        String workingDir = System.getProperty("user.dir");
+        Path savePath = Paths.get(workingDir, "SessionSaves");
+        try {
+            if (Files.notExists(savePath)) {
+                Files.createDirectory(savePath);
+            }
+        } catch (IOException e) {
+            logger.error("Caught IO Exception creating SaveSessions directory ", e);
+        }
+        Path filePath = savePath.resolve(sessionName + ".json");
         ObjectMapper mapper = new ObjectMapper();
         try {
-            mapper.writerWithDefaultPrettyPrinter().writeValue(myFile, speedTrapSessionWrapper);
-            logger.info("Data saved successfully to {}", myFile.getName());
+            mapper.writerWithDefaultPrettyPrinter().writeValue(filePath.toFile(), speedTrapSessionWrapper);
+            logger.info("Data saved successfully to {}", filePath.toFile().getName());
         } catch (IOException e) {
             logger.error("Caught IO Exception ", e);
         }
