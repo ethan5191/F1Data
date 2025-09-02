d3200f2e04bedf28945be64e1910dd5a413e0851
ethan5191 <ethan5191@hotmail.com>
2025-09-02 12:57:57 -0400

    Updated build.bat file to do a java check to see if its even installed. if it is installed I am assuming its at least 17 for now. Printing out the java version so that if an error occurs, we should be able to easily find out if its a java version problem

--- build.bat
+++ build.bat
@@ -1,5 +1,13 @@
 @echo off
-echo Starting the build process...
-mvnw clean package -DskipTests
-echo Build finished. Press any key to exit.
-pause >nul
+where java >nul 2>nul
+if %errorlevel%==1 (
+    @echo Java not found in path, please install at least Java 17 and try again.
+    pause
+) else (
+    echo java found:
+    java --version
+    echo Starting the build process...
+    call mvnw clean package -DskipTests
+    echo Build finished. Press any key to exit.
+    pause >nul
+)
