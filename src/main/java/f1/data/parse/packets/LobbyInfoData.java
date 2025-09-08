2118e348a012e6322b428025995d160681dc9c8c
ethan5191 <ethan5191@hotmail.com>
2025-09-08 19:26:35 -0400

    Forgot to update the block comment.

--- src/main/java/f1/data/parse/packets/LobbyInfoData.java
+++ src/main/java/f1/data/parse/packets/LobbyInfoData.java
@@ -7,7 +7,7 @@ import java.nio.ByteBuffer;
 /**
  * F1 2020 LobbyInfoData Breakdown (Little Endian)
  * - F1 2020 Length: 52 bytes per player
- * - F1 2021 Length: 53 bytes per player
+ * - F1 2021/2022 Length: 53 bytes per player
  * This struct is 52 bytes long and contains details about a single player in the lobby,
  * including their name, team, and ready status. This data is sent as an array for each player.
  * <p>
