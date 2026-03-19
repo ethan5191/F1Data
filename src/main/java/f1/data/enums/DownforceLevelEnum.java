54cb815a703edb30630e3b7acfb7412ef9de5fbd
ethan5191 <ethan5191@hotmail.com>
2026-03-18 21:46:29 -0400

    Updated F2 setups for 2023 and simplified the downforceLevelEnum.

--- src/main/java/f1/data/enums/DownforceLevelEnum.java
+++ src/main/java/f1/data/enums/DownforceLevelEnum.java
@@ -2,26 +2,20 @@ package f1.data.enums;
 
 //Enum is a definition of the downforce levels for AI setups based on track needs.
 public enum DownforceLevelEnum {
-
-    MONZA("MONZA"),
-    LOW("LOW"),
-    MEDIUM("MEDIUM"),
-    HIGH("HIGH"),
-    MAX("MAX"),
-    UNKNOWN("UNKNOWN"),
-    F2_MONZA("F2.MONZA"),
-    F2_LOW("F2_LOW"),
-    F2_MEDIUM("F2_MEDIUM"),
-    F2_HIGH("F2_HIGH"),
-    F2_MAX("F2_MAX");
-
-    private final String name;
-
-    DownforceLevelEnum(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return name;
-    }
+    MONZA,
+    LOW,
+    MEDIUM,
+    HIGH,
+    MAX,
+    F2_MONZA,
+    F2_LOW,
+    F2_MEDIUM,
+    F2_HIGH,
+    F2_MAX,
+    PREV_F2_MONZA,
+    PREV_F2_LOW,
+    PREV_F2_MEDIUM,
+    PREV_F2_HIGH,
+    PREV_F2_MAX,
+    UNKNOWN;
 }
